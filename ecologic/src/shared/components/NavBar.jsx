eabe5f6098379b0ecd5235f2621e45c39d4c6de5
Rodrigo Almeida Dourado <115838474+Rodrigo-Dourad0@users.noreply.github.com>
2026-07-07 10:30:27 -0300

    Retira o botao de notificações do navbar

--- ecologic/src/shared/components/NavBar.jsx
+++ ecologic/src/shared/components/NavBar.jsx
@@ -78,15 +78,7 @@ const NavBar = ({ currentPage, onNavigate, toggleMobileMenu }) => {
           />
         </div>
 
-        {/* Botão de Notificações */}
-        <button className="navbar-notification-btn" aria-label="Notificações">
-          <svg viewBox="0 0 24 24" width="24" height="24">
-            <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path>
-            <path d="M13.73 21a2 2 0 0 1-3.46 0"></path>
-          </svg>
-          <span className="navbar-notification-dot"></span>
-        </button>
-
+    
         {/* Bloco de Perfil do Usuário */}
         <div className="navbar-profile-wrapper" onClick={() => onNavigate('perfil')}>
           <div className="navbar-profile-info">
